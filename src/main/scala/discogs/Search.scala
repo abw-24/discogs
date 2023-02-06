d4b2a536641e394deeeb5c3c4d915626342e3a5c
awest-24 <awest.uchi@gmail.com>
2023-02-06 13:25:50 -0600

    basic parsing of JSON results implemented with play library. should obviously be moved out of main when data models are available

--- src/main/scala/discogs/Search.scala
+++ src/main/scala/discogs/Search.scala
@@ -1,13 +1,21 @@
 package discogs
 
 import discogs.client.UserTokenDiscogsClient
+import play.api.libs.json.Json
 
 object Search {
   def main(args: Array[String]): Unit = {
+    // Pop token from query term arguments, create a client
     val token :: queryList = args.toList
     val client = UserTokenDiscogsClient(token)
+    // Build simple query from term list
     val queryBuilder = new StringBuilder()
-    val queryString = queryList.addString(queryBuilder , " ").toString
-    println(client.search(queryString))
+    val queryString = queryList.addString(queryBuilder , "&").toString
+    // Use the client to send query, parse result
+    val rawJs = Json.parse(client.search(queryString))
+    val resultsField = rawJs \\ "results"
+    // Print the first element from the first page of results
+    val firstPage = resultsField.head
+    println(Json.prettyPrint(firstPage(0)))
   }
 }
--- src/main/scala/discogs/fetch/Fetch.scala
+++ src/main/scala/discogs/fetch/Fetch.scala
@@ -6,13 +6,14 @@ import scala.concurrent._, duration._
 trait Fetch {
   def fetch(url: String, http: HttpClient): String = {
     val rawRequest = Gigahorse.url(url).get
-    val future = http.processFull(rawRequest, Gigahorse.asString andThen {_.take(100)})
+    val future = http.processFull(rawRequest, Gigahorse.asString)
+    //Move blocking further upstream?
     Await.result(future, 30.seconds)
   }
 }
 
 class UserTokenFetcher(token: String, http: HttpClient) extends Fetch {
-  private[this] val discogsAuthStr = s"&token=$token"
+  final private[this] val discogsAuthStr = s"&token=$token"
   def fetch(url: String): String = super.fetch(url + discogsAuthStr, http)
 }
 
@@ -22,7 +23,7 @@ object UserTokenFetcher {
 }
 
 class KeySecretFetcher(key: String, secret: String, http: HttpClient) extends Fetch {
-  private[this] val discogsAuthStr = s"&key=$key&secret=$secret"
+  final private[this] val discogsAuthStr = s"&key=$key&secret=$secret"
   def fetch(url: String): String = super.fetch(url + discogsAuthStr, http)
 }
 
